0d7fd0449594270103576a0c463707a31129fd00
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-01-04 14:31:01 +0100

    fix: uninstalled 'flowbite-react'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-bg': "url('./src/assets/hero-bg.png')",
-      }
+        "hero-bg": "url('./src/assets/hero-bg.png')",
+      },
     },
 
     fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui',],
-      'serif': ['ui-serif', 'Georgia',],
-      'mono': ['ui-monospace', 'monospace',],
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "monospace"],
     },
 
     fontWeight: {
-      thin: '100',
-      extralight: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semibold: '600',
-      bold: '700',
-      extrabold: '800',
-      black: '900',
+      thin: "100",
+      extralight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      black: "900",
     },
-
   },
-  plugins: [require("daisyui"), require('flowbite/plugin')],
+  plugins: [require("daisyui")],
 };
