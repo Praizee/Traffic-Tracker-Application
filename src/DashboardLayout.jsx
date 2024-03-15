abcd8e1239bec175b5c010b6013f99e95d64fef2
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-03-15 13:50:07 +0100

    chore: navbar

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -199,7 +199,7 @@ const DashboardLayout = () => {
       </Drawer>
 
       <Layout className={collapsed ? "md:ml-[80px]" : "md:ml-[200px]"}>
-        <Header className="border-b-2 fixed z-[1000] w-full p-0 bg-white flex items-center">
+        <Header className="border-b-2 fixed z-[1000] w-full p-0 bg-white flex items-cente">
           <Button
             type="text"
             icon={collapsed ? <AiOutlineMenuUnfold /> : <AiOutlineMenuFold />}
@@ -211,19 +211,159 @@ const DashboardLayout = () => {
             }}
             className="md:hidden block"
           />
-          <div className="hidden md:flex flex-wrap gap-8 px-6 md:pl-24">
-            <a className="duration-200 transition hover:font-medium hover:text-blue-600">
-              Live Traffic
-            </a>
-            <a className="duration-200 transition hover:font-medium hover:text-blue-600">
-              Trip
-            </a>
-            <a className="duration-200 transition hover:font-medium hover:text-blue-600">
-              Reports
-            </a>
-            <a className="duration-200 transition hover:font-medium hover:text-blue-600">
-              Emergency
-            </a>
+
+          <div
+            // className="flex px-6 md:pl-12 mx-8 w-full max-w-[1124px]"
+            className={`flex px-6 md:pl-12 mx-8 ${
+              collapsed ? "max-w-[1224px]" : "max-w-[1124px]"
+            } w-full`}
+          >
+            <div className="hidden lg:flex gap-8 flex-1">
+              <a className="duration-200 transition hover:font-medium hover:text-blue-600">
+                Live Traffic
+              </a>
+              <a className="duration-200 transition hover:font-medium hover:text-blue-600">
+                Trip
+              </a>
+              <a className="duration-200 transition hover:font-medium hover:text-blue-600">
+                Reports
+              </a>
+              <a className="duration-200 transition hover:font-medium hover:text-blue-600">
+                Community
+              </a>
+              <a className="duration-200 transition hover:font-medium hover:text-blue-600">
+                Emergency
+              </a>
+            </div>
+
+            <div className="flex gap-4 flex-none">
+              {/* notification */}
+              <div className="dropdown dropdown-end mt-1.5">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                        />
+                      </svg>
+                      <span className="badge badge-sm badge-error text-white indicator-item">
+                        5
+                      </span>
+                    </div>
+                  </button>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="mt-1 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <p className="text-base font-medium">Notification 1</p>
+                  </li>
+                  <li>
+                    <p className="text-base font-medium">Notification 2</p>
+                  </li>
+                  <li>
+                    <p className="text-base font-medium">Notification 3</p>
+                  </li>
+                </ul>
+              </div>
+
+              {/* search */}
+              <form className="relative">
+                <label for="Search" className="sr-only">
+                  Search
+                </label>
+                <input
+                  type="text"
+                  id="Search"
+                  placeholder="Search Area"
+                  required
+                  className="w-full rounded-[30px] border border-gray-200 bg-[#F5F8FF] text-[#84848466] py-2.5 pe-10 pl-9 shadow-sm sm:text-sm"
+                />
+                {/* search icon */}
+                <span className="absolute inset-y-0 start-0 grid w-10 place-content-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#84848466"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                  </svg>
+                </span>
+                {/* submit */}
+                <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+                  <button
+                    type="submit"
+                    className="text-gray-600 hover:text-gray-700"
+                  >
+                    <span className="sr-only">Search</span>
+                    <svg
+                      width="14"
+                      height="13"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9.88476 7.35324H0.847656V5.64699H9.88476L5.78341 1.54563L7.00011 0.347656L13.1526 6.50011L7.00011 12.6526L5.78341 11.4546L9.88476 7.35324Z"
+                        fill="#C8CACE"
+                      />
+                    </svg>
+                  </button>
+                </span>
+              </form>
+
+              {/* avatar */}
+              <div className="dropdown dropdown-end mt-1.5">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img
+                      alt="Avatar"
+                      src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="mt-1 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-32"
+                >
+                  <li>
+                    <a className="text-base font-medium">Profile</a>
+                  </li>
+                  <li>
+                    <a className="text-base font-medium">Settings</a>
+                  </li>
+                  <li>
+                    <a className="text-base font-medium">Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </Header>
 
