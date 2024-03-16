cc24c17ce2e5b47add99e68a886665841901f537
Bridget Amana <amanabridget03@gmail.com>
2024-03-16 18:14:29 +0100

    feat: favorite-routes page

---
+++ src/Pages/FavoriteRoutes/FavoutiteRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import TripItem from '../../Components/TripItem/TripItem'
+const FavoutiteRoutes = () => {
+  return (
+    <div className="md:p-8 p-3">
+    <h2 className="text-3xl font-semibold mb-8 hidden md:flex md:mx-auto">Favorite Routes</h2>
+    <section className="max-w-screen-lg mx-auto py-6">
+      <TripItem/>
+      <TripItem />
+      <TripItem/>
+      <TripItem/>
+      <TripItem />
+      <TripItem/>
+      <TripItem />
+      <TripItem />
+      <TripItem/>
+      <TripItem/>
+    </section>
+  </div>
+
+  )
+}
+
+export default FavoutiteRoutes
