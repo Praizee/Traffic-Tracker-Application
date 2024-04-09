4209af9a3575a5af8ad8cfb3d2de5172d4ce48cb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-04-09 22:43:03 +0100

    fix: notifications

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -257,7 +257,7 @@ const DashboardLayout = () => {
 
             <div className="flex gap-4 justify-between md:justify-normal lg:flex-none">
               {/* notification */}
-              <div className="dropdown dropdown-right mt-1.5">
+              <div className="dropdown dropdown-end mt-1.5 hidden md:block">
                 <div
                   tabIndex={0}
                   role="button"
@@ -379,6 +379,57 @@ const DashboardLayout = () => {
                     />
                   </svg>
                 </label>
+              </div>
+
+              {/* notification */}
+              <div className="dropdown dropdown-end mt-1.5 md:hidden block">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                        />
+                      </svg>
+                      <span className="badge badge-sm badge-error text-white indicator-item">
+                        5
+                      </span>
+                    </div>
+                  </button>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="mt-1 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link to="notifications" className="text-base font-medium">
+                      Notification 1
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="notifications" className="text-base font-medium">
+                      Notification 2
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="notifications" className="text-base font-medium">
+                      Notification 3
+                    </Link>
+                  </li>
+                </ul>
               </div>
 
               {/* avatar */}
